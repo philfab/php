f094f6a280d5ce29bb14850f28c2fbe2077e7532
chessoft <chessoft@hotmail.com>
2024-03-05 15:18:03 +0100

    exo 13 ok

---
+++ exo13.php
@@ -0,0 +1,22 @@
+<h1>
+    Exo php 12
+</h1>
+
+<p>
+    Calculer la moyenne générale d'un élève dont les notes sont renseignées dans un tableau (pas de coefficient). Elle devra être affichée avec 2 décimales.
+</p>
+
+<h2>
+    Résultat
+</h2>
+
+<?php
+$notes = array(10, 12, 8, 19, 3, 16, 11, 13, 9);
+$moyenne = round(array_sum($notes) / count($notes),2);
+
+echo "Les notes obtenues par l’élève sont : ";
+
+foreach ($notes as $note)
+    echo "$note ";
+
+echo "<br>Sa moyenne générale est donc de : $moyenne";
