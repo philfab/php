4154bb9465ce1fd8a5e1eb721710fa5b32229374
chessoft <chessoft@hotmail.com>
2024-03-05 14:15:36 +0100

    exo 10 ok

---
+++ exo10.php
@@ -0,0 +1,53 @@
+<h1>
+    Exo php 10
+</h1>
+
+<p>
+    A partir d’un montant à payer et d’une somme versée pour régler un achat, <br>
+    écrire l’algorithme qui affiche à un utilisateur un rendu de monnaie en nombre de billets de 10 € et 5 €, de pièces de 2 € et 1 €.
+</p>
+
+<h2>
+    Résultat
+</h2>
+
+<?php
+
+$aPayer = 152;
+$Verse = 200;
+
+echo "Montant à payer : $aPayer €<br>";
+echo "Montant versé : $Verse €<br>";
+echo "Reste à rendre : " . ($Verse - $aPayer) . " €<br>"; // . . > concat chaines
+
+rendu($aPayer, $Verse);
+function rendu($mAPayer, $mVerse)
+{
+
+    $reste = $mVerse - $mAPayer;
+
+    $billet10 = 0;
+    $billet5 = 0;
+    $piece2 = 0;
+    $piece1 = 0;
+
+    while ($reste > 0) {
+        if ($reste >= 10) {
+            $billet10++;
+            $reste -= 10;
+        } elseif ($reste >= 5) {
+            $billet5++;
+            $reste -= 5;
+        } elseif ($reste >= 2) {
+            $piece2++;
+            $reste -= 2;
+        } else {
+            $piece1++;
+            $reste -= 1;
+        }
+    }
+
+    echo "Rendu :<br>";
+    echo "$billet10 billet(s) de 10€ , $billet5 billet(s) de 5€ , $piece2 pièce(s) de 2€ , $piece1 pièce(s) de 1€";
+}
+?>
