1c554ce3771d8e987cf1192610b9ae0c2a4f00c8
chessoft <chessoft@hotmail.com>
2024-03-05 14:38:59 +0100

    exo 11 ok

--- exo11.php
+++ exo11.php
@@ -3,7 +3,7 @@
 </h1>
 
 <p>
-
+    Initialiser un tableau de x marques de voitures. Ecrire un algorithme permettant de parcourir ce tableau et d’en afficher le <br> contenu (une marque de voiture par ligne). Il est également demandé d’afficher le nombre de marques de voitures présentes dans le tableau.
 </p>
 
 <h2>
@@ -11,5 +11,13 @@
 </h2>
 
 <?php
+$tabMarques = array("Peugeot", "Renault", "BMW", "Mercedes");
+$nMarques = count($tabMarques);
 
-?>
+echo "Il y a $nMarques marques de voitures dans le tableau : <br><ul>";
+
+foreach ($tabMarques as $marque) {;
+    echo "<li>$marque</li>";
+}
+
+echo "</ul>";
