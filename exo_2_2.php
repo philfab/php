f863e7e2c25daa279bb7cac792e332a3a3659397
philfab <mpemploipo@gmail.com>
2024-03-07 09:34:28 +0100

    exo php 2.2 ok

---
+++ exo_2_2.php
@@ -0,0 +1,40 @@
+<h1>
+    Exo php 2.2
+</h1>
+
+<p>
+    Réaliser un algorithme permettant d’afficher le tableau HTML suivant (notez que le nom du pays <br>
+    s’affichera en majuscule et que le tableau est trié par ordre alphabétique du nom de pays) grâce à<br>
+    une fonction personnalisée
+</p>
+
+<h2>
+    Résultat
+</h2>
+
+<?php
+
+$capitales = ["France" => "Paris", "Allemagne" => "Berlin", "USA" => "Washington", "Italie" => "Rome"];
+asort($capitales); //tri ascendant par rapport à la valeur (ksort = rapport à la clef)
+echo '<table  border="1">';
+echo afficherTableHTML($capitales);
+echo "</table>";
+function afficherTableHTML($capitales)
+{
+    echo "
+    <tr>
+      <th>Pays</td>
+      <th>Capitale</td>
+    </tr>
+     ";
+    foreach ($capitales as $pays => $ville) {
+        echo "
+        <tr>
+         <td>" . mb_strtoupper($pays) . "</td>
+         <td>$ville</td>
+        </tr>
+        ";
+    }
+}
+
+?>
